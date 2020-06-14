fbfa1405a7e82c2dad9d0d117d9024bd478992e5
jonaspetraska7 <jonas.petraska7@gmail.com>
2020-06-14 17:17:53 +0300

    adding details
    adding detailed view functionality for the app

--- movieapp/App.js
+++ movieapp/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View, TextInput, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, Image, TouchableHighlight, Modal } from 'react-native';
 
 
 
 export default function App() {
   const apiurl = "https://api.themoviedb.org/3/search/movie?api_key=d24c52c68333a633aa24bd3c71091439"
-  const imageroot = "http://image.tmdb.org/t/p/original"
+  const imageroot = "https://image.tmdb.org/t/p/original"
+  const searchroot = "https://api.themoviedb.org/3/movie/"
+  const searchend = "?api_key=d24c52c68333a633aa24bd3c71091439"
   const [state, setState] = useState({
     s: "Enter a movie...",
     results: [],
@@ -16,9 +18,19 @@ export default function App() {
   const search = () => {
     axios(apiurl + "&query=" + state.s).then(({ data }) => {
       let results = data.results
-      console.log(results)
+      //console.log(results)
       setState(prevState => {
         return { ...prevState, results: results }
+      })
+    })
+  }
+
+  const openPopup = id => {
+    axios(searchroot + id + searchend).then(({ data }) => {
+      let result = data
+      //console.log(result)
+      setState(prevState => {
+        return {...prevState, selected: result }
       })
     })
   }
@@ -34,8 +46,9 @@ export default function App() {
         />
         <ScrollView style={styles.results}>
           {Object.keys(state.results).map(result => (
+            <TouchableHighlight key={state.results[result].id} onPress={ () => openPopup(state.results[result].id)}>
             <View style={styles.result}>
-              {console.log(state.results[result].poster_path)}
+              {/*console.log(state.results[result].poster_path)*/}
               <Image 
                 source={{ uri: imageroot + state.results[result].poster_path}}
                 style={{
@@ -48,9 +61,31 @@ export default function App() {
                 {state.results[result].original_title}
               </Text>
             </View>
+            </TouchableHighlight>
             ))}
 
         </ScrollView>
+
+        <Modal 
+          animationType="fade"
+          transparent={false}
+          visible={ (state.selected == "undefined") ? false : true }
+        >
+          {console.log(state.selected.original_title)}
+          <View style={styles.popup}>
+            <Text style={styles.poptitle}>{state.selected.original_title}</Text>
+            <Text style={{marginBottom: 20}}>Rating : {state.selected.vote_average} </Text>
+            <Text>{state.selected.overview}</Text>
+          </View>
+          <TouchableHighlight
+            onPress= { () => setState(prevState => {
+              return {...prevState, selected: "undefined" }
+            })}>
+              <Text style={styles.backBtn}>Back</Text>
+            </TouchableHighlight>
+
+            
+        </Modal>
     </View>
   );
 }
@@ -82,5 +117,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '700',
     padding: 20
+  },
+  popup: {
+    padding: 20
+  },
+  poptitle: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 5
+  },
+  backBtn: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: '700',
   }
 });
